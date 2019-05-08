5c599c49e6ee1da972690604359dba934cf1b5e7
Jacttis <rojoman@hotmail.com.ar>
2019-05-08 14:49:24 -0300

    chanfe

--- bot.js
+++ bot.js
@@ -5,6 +5,16 @@ const client = new Discord.Client();
 client.on('ready', () => {
     console.log(`bot is ready ${client.user.tag}!`);
 });
+
+client.on('guildMemberAdd', member => {
+    // Send the message to a designated channel on a server:
+    const channel = member.guild.channels.find(ch => ch.name === 'member-log');
+    // Do nothing if the channel wasn't found on this server
+    if (!channel) return;
+    // Send the message, mentioning the member
+    channel.send(`Bienvenido a VRG5, ${member}`);
+});
+
 client.on('message', (msg) => {
         console.log(msg.content);
         if(msg == '/topo'){
